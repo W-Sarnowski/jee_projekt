f6e6b66e3792b8a5f44056e66ad1b6f2d608a3ae
W-Sarnowski <sarnowskiwojtek5500@gmail.com>
2024-01-15 20:35:20 +0100

    basic navbar, different palette, imported material

--- front-app/src/app/app-routing.module.ts
+++ front-app/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { BrowserModule } from '@angular/platform-browser';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', component: HomePageComponent},
+  {path: 'home', pathMatch: 'full', component: HomePageComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- front-app/src/app/app.module.ts
+++ front-app/src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -18,9 +23,16 @@ import { NavbarComponent } from './navbar/navbar.component';
     BrowserModule,
     AppRoutingModule,
     CommonModule,
-    RouterOutlet,
+    RouterModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MaterialModule,
   ],
-  providers: [],
+  providers: [
+    RouterOutlet
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
